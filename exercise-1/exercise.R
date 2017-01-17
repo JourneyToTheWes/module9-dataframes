a2fa33b66f98a8d5a20ac04b0ed5a5e2c81832b0
Wesley Wei <wesley631w@gmail.com>
2017-01-17 15:09:46 -0800

    Practiced with data frames with seahawks stats

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,19 +2,19 @@
 
 # Create a vector of the number of points the Seahawks scored in each game this
 # season (google "Seahawks" for the info!)
-
+seahawks.num.points <- c(20, 31, 31, 26, 5, 40, 10, 24, 31, 25, 26, 20)
 
 # Create a vector of the number of points the Seahwaks have allowed to be scored
 # against them in each game this season
-
+seahawks.opposing.points <- c(25, 25, 24, 15, 14, 7, 38, 3, 34, 23, 6, 36)
 
 # Combine your two vectors into a dataframe
-
+seahawks.score.data <- data.frame(seahawks.num.points, seahawks.opposing.points)
 
 # Create a new column "diff" that is the difference in points.
 # Hint: recall the syntax for assigning new elements (which in this case will be
 # a vector) to a list!
-
+seahawks.score.data <- 
 
 # Create a new column "won" which is TRUE if the Seahawks won
 
