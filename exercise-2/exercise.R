5c05949a028da681cab99c0f999c0c3f401003c6
Wesley Wei <wesley631w@gmail.com>
2017-01-17 15:40:15 -0800

    Practice with more data frames - made data frame for employees and salaries

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,19 +1,19 @@
 # Create a vector of 100 employees ("Employee 1", "Employee 2", ... "Employee 100)
 # Hint: use the `paste()` function to produce the list
-
+employees <- c(paste("Employee", seq(1, 100)))
 
 # Create a vector of 100 random salaries for the year 2014
 # Use the `runif()` function to pick a random number between 40000 and 50000
-
+salaries.in.2014 <- c(runif(100, 40000, 50000))
 
 # Create a vector of 100 salaries in 2015 that have increased from 2014 by some amount
 # Hint: use `runif()` to add a random number to 2014's salaries. Starting from a
 # _negative_ number so that salaries may decrease!
-
+salaries.in.2015 <- c(runif(100, 30000, 60000))
 
 # Create a data.frame 'salaries' by combining the 3 vectors you just made
 # Remember to set `stringsAsFactors=FALSE`!
-
+salaries <- data.frame(employees, salaries.in.2014, salaries.in.2015)
 
 # Create a column 'raise' that stores the size of the raise between 2014 and 2015
 
